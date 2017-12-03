5db97e1f9917f608974e6bea91ffdea3021ace10
James Kyle <me@thejameskyle.com>
2017-12-03 17:51:01 +1100

    Mobile friendly and utf-8

--- nprlite.go
+++ nprlite.go
@@ -21,6 +21,9 @@ const (
 <html>
 <head>
 %s
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
 <style>
 body {
 	max-width: 650px;
